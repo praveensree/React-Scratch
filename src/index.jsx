ebc57df25f28f80747ebe04fd57c6e0f4084303e
Praveen P <praveen.padmanabhan@bhavnacorp.com>
2022-02-18 12:22:05 +0530

    use effect

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './Home.jsx'
-import App from './App.jsx'
+import Posts from "./Posts.jsx";
+
+function Home() {
+    return (
+      <div >
+          <h1>Home Page</h1>
+        <Posts/>
+      </div>
+    );
+  }
 
 ReactDOM.render(<Home/>, document.getElementById('app'));
